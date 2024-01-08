09f06f661bb7ef54ea70c0332c10253056dffa85
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2024-01-08 17:54:22 +0630

    fix:Fix hello & saiHello Controller

--- src/main/java/com/devtiro/quickstart/HelloSaiHlyanPhyoeController.java
+++ src/main/java/com/devtiro/quickstart/HelloSaiHlyanPhyoeController.java
@@ -1,2 +1,13 @@
-package com.devtiro.quickstart;public class HelloSaiHlyanPhyoe {
+package com.devtiro.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSaiHlyanPhyoeController {
+
+    @GetMapping(path = "/sai")
+    public String helloSaiHlyanPhyoe() {
+        return "Hello Sai Hlyan Phyoe";
+    }
 }
